860027a1c8000def7bbce7bbddf26b5e9e0b403b
pejiccivaa-ui <pejiccivaa@gmail.com>
2025-09-09 20:29:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const heartsContainer = document.getElementById('hearts');
 sendBtn.addEventListener('click', () => {
   const text = userInput.value.trim();
   if(text) {
-    message.textContent = `Tvoja poruka: "${text}" ❤️`;
+    message.textContent = `Tvoja poruka: "${text}" 💕`;
     createHearts(5);
     userInput.value = '';
   } else {
-    message.textContent = "Upiši nešto prvo! 😘";
+    message.textContent = "Upiši nešto prvo! 💖";
   }
 });
 
@@ -18,7 +18,7 @@ function createHearts(count) {
   for(let i=0; i<count; i++){
     const heart = document.createElement('div');
     heart.classList.add('heart');
-    heart.textContent = '💖';
+    heart.textContent = '💗';
     heart.style.left = `${Math.random() * 80 + 10}%`;
     heartsContainer.appendChild(heart);
 
